b43c4629670a13b068f091733b72a009f731d3af
Fiona <h.natapra@gmail.com>
2017-03-24 16:11:21 -0400

    finished

--- src/Validator.js
+++ src/Validator.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import "./Validator.css";
 
 class Validator extends Component {
+  constructor(props) {
+    super()
+  }
+  validatePassword() {
+
+    let x=document.forms["myForm"]["password"].value;
+    let y=document.forms["myForm"]["confirm"].value;
+
+      if (x !== y) {
+        alert("Passwords must match")
+        return false;
+      } else {
+        alert("Valid!")
+        return true;
+      }
+
+  }
+
   render() {
     return (
       <div className="form">
+        <form name="myForm">
         <h1>Sign Up</h1>
         <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <input type="password" placeholder="confirm password" />
-        <input type="submit" value="Submit" />
+        <input type="password" name="password" placeholder="password" />
+        <input type="password" name="confirm" placeholder="confirm password" />
+        <button onClick={() => this.validatePassword()}> Submit </button>
+        </form>
       </div>
     );
   }
